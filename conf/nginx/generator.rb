9c0cc910f8fda07ffe31115d073ae60e7f128d62
Bruno Michel <bmichel@menfin.info>
2012-05-01 21:21:39 +0200

    Allow the inclusion of LinuxFr.org in an iframe

--- conf/nginx/generator.rb
+++ conf/nginx/generator.rb
@@ -55,8 +55,6 @@ server {
     ssl_session_cache shared:SSL:2m;
     ssl_ciphers HIGH:!aNULL:!MD5:!kEDH;
 
-    add_header X-Frame-Options DENY;
-
     proxy_max_temp_file_size 0;
     client_max_body_size 2M;
 
