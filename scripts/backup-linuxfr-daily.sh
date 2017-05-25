d3c4b2e992e4ded77d2013efe017a88d3def468b
Nils <nils.github@anotherhomepage.org>
2017-05-25 21:57:00 +0200

    Delete backup-linuxfr-daily.sh
    It has been replaced with another script

--- scripts/backup-linuxfr-daily.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-. "conf.sh"
-# sauvegarde sql journalière
-mysqldump ${DATABASE} | gzip > ${BACKUP_DIR}/linuxfr-daily.dump.gz
-mysqldump -F mysql --events | gzip > ${BACKUP_DIR}/mysql-daily.dump.gz
-gzip -c /var/lib/redis/dump.rdb > ${BACKUP_DIR}/redis.rdb.gz
-
